dcbd1266975271475a104a34cd750703a6b521d4
hodorgeek <sham@hodrogeek.com>
2018-02-28 20:40:48 +0530

    created first component

--- react-js/reactjs-basics/src/app/index.js
+++ react-js/reactjs-basics/src/app/index.js
@@ -1,5 +1,23 @@
-/**
- * Created by sham on 15-02-2018.
- */
+import React from "react";
+import {render} from "react-dom";
 
-console.log("inside index.js");
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Hello!</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
+
+
+
+
